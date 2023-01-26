f464d5f81dfb984ba893f81090164ae87d3f9ead
Lars901 <larsove_hoines@outlook.com>
2023-01-26 17:41:47 +0100

    Update youtube.sh

--- youtube.sh
+++ youtube.sh
@@ -5,5 +5,5 @@ sudo apt install ffmpeg -y
 sudo apt install python -y
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
-wget https://dl.4kdownload.com/app/4kvideodownloader_4.23.0-1_amd64.deb?source=website
+wget https://dl.4kdownload.com/app/4kvideodownloader_4.23.0-1_amd64.deb
 sudo dpkg -i 4kvideodownloader_4.23.0-1_amd64.deb
