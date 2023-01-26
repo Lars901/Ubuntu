31fa2e7f2fd74ff4d693ca7d538d269a732892bd
Lars901 <larsove_hoines@outlook.com>
2023-01-26 17:30:38 +0100

    Programmer

---
+++ Program til å laste ned fra youtube.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#Installerer programmer som kreves før selve programmet.
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install ffmpeg -y
+sudo apt install python -y
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+wget https://dl.4kdownload.com/app/4kvideodownloader_4.23.0-1_amd64.deb?source=website
+sudo dpkg -i 4kvideodownloader_4.23.0-1_amd64.deb
