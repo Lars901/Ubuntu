1bf76eb4ffa3ac1467f26cc0350912f8ff5779c7
Lars901 <larsove_hoines@outlook.com>
2023-04-08 22:28:09 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo apt-mark hold snapd -y
 snap list
 sudo snap remove firefox
 sudo apt install plasma-discover-backend-flatpak
+sudo add-apt repository universe
 
 #extra programs
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
