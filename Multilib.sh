9fd44d2890ed1d680e1d5d5919029f4c01bf1b55
Lars901 <larsove_hoines@outlook.com>
2023-04-08 23:57:41 +0200

    cleanup

--- Multilib.sh
+++ Multilib.sh
@@ -5,3 +5,4 @@ sudo apt install -y steam
 sudo apt install -y mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
 sudo apt install -y winetricks
 sudo apt install -y libxtst6:i386 libxrandr2:i386 libglib2.0-0:i386 libgtk2.0-0:i386 libpulse0:i386 libgdk-pixbuf2.0-0:i386 libcurl4-openssl-dev:i386 libopenal1:i386 libusb-1.0-0:i386 libdbus-glib-1-2:i386 
+sudo apt install -y wget gdebi-core libgl1-mesa-glx:i386
--- install.sh
+++ install.sh
@@ -9,13 +9,135 @@ flatpak install flathub org.DolphinEmu.dolphin-emu
 #Universe Repo
 sudo add-apt repository universe
 
+# Graphics Drivers find and install
+if lspci | grep -E "NVIDIA|GeForce"; then
+    sudo apt -y install nvidia
+	nvidia-xconfig
+elif lspci | grep -E "Radeon"; then
+    sudo apt -y install xserver-xorg-video-amdgpu firmware-amd-graphics
+elif lspci | grep -E "Integrated Graphics Controller"; then
+    sudo apt -y install libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
+fi
+PKGS=(
+'alsa-utils' # audio utils
+'ark' # compression
+'apt-transport-https' #Dependecy for Brave
+'kio-audiocd' 
+'kitty'
+'autoconf' # build
+'automake' # build
+'bash-completion'
+'binutils'
+'bison'
+'bluedevil'
+'bluez'
+'breeze'
+'btrfs-progs'
+'celluloid' # video players
+'cups'
+'curl'
+'dialog'
+'dosfstools'
+'exfat-utils'
+'fonts-terminus'
+'flex'
+'fuse3'
+'fuseiso'
+'gamemode'
+'gdebi-core'
+'gcc'
+'gdisk'
+'grub-customizer'
+'haveged'
+'htop'
+'nftables'
+'inkscape' #Vector drawing
+#'openjdk-17-jdk' # Java 17
+#'openjdk-17-jre' #Java 17 jre
+'qt5-style-kvantum'
+'libnewt-dev'
+'libtool'
+'lsof'
+'lutris'
+'lzop'
+'m4'
+'make'
+'milou'
+'nano'
+'neofetch'
+'ntfs-3g'
+'ntp'
+'okular'
+'obs-studio'
+'netcat-openbsd'
+'os-prober'
+'obs-studio'
+'p7zip'
+'patch'
+'pkgconf' 
+'print-manager'
+'python3-pip'
+'qemu-kvm'
+'libvirt-clients' #Dependecy for quemu
+'libvirt-daemon-system' #Dependecy for quemu
+'bridge-utils' #Dependecy for quemu
+'virtinst' #Dependecy for virt-manager
+'libvirt-daemon' #Dependecy for virt-manager
+'virt-manager' 
+'rsync'
+'snapper'
+'steam'
+'systemsettings'
+'fonts-terminus'
+'flatpak'
+'flameshot'
+'telegram-desktop'
+'traceroute'
+'telegram-desktop' #Instant messaging client
+'ufw'
+'unrar'
+'unzip'
+'usbutils'
+'vulkan-tools'
+'virt-manager'
+'virt-viewer'
+'wget'
+'kde-zeroconf'
+'ktorrent'
+'kde-baseapps'
+'zip'
+'g++'
+'libx11-dev'
+'libxext-dev'
+'qtbase5-dev'
+'libqt5svg5-dev'
+'libqt5x11extras5-dev'
+'libkf5windowsystem-dev'
+'qttools5-dev-tools'
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo apt -y install "$PKG"
+done
+
+sudo dpkg-reconfigure console-setup
+#sudo systemctl status libvirtd.service
+sudo adduser $User libvirt
+sudo adduser $User libvirt-qemu
+sudo -v && wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sudo sh /dev/stdin
+
+sudo apt install plasma-discover-backend-flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+#Dolphin Emu
+flatpak install flathub org.DolphinEmu.dolphin-emu
+
 #extra programs
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg | gpg --dearmor | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://download.vscodium.com/debs vscodium main' | sudo tee /etc/apt/sources.list.d/vscodium.list
 sudo apt update
 sudo apt install codium -y
-#Inkscape
-sudo apt install inkscape -y
+
 #brave
 sudo apt install curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
@@ -26,18 +148,7 @@ sudo apt install brave-browser
 echo "deb http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list; wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg
 sudo apt update && sudo apt install nala -y
 sudo nala fetch
-#KVM 
-sudo apt -y install qemu-kvm libvirt-daemon bridge-utils virtinst libvirt-daemon-system
-sudo apt -y install virt-top libguestfs-tools libosinfo-bin  qemu-system virt-manager
-sudo modprobe vhost_net
-sudo tee  /etc/apt/sources.list.d/swtpm-tools.list<<EOF
-deb [trusted=yes] http://ppa.launchpad.net/stefanberger/swtpm-jammy/ubuntu jammy main
-EOF
-sudo apt update -y && sudo apt -y install ovmf swtpm swtpm-tools virt-manager virt-viewer
 
 #Fonts
 sudo apt-get install -y ttf-mscorefonts-installer
 wget -qO- http://plasmasturm.org/dl/vistafonts-installer | bash
-
-#Telegram
-sudo apt install -y telegram-desktop
